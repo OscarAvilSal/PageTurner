f6e74e92f4a786af23942af5a580fd5701d15905
Justin <justin.andreoli04@gmail.com>
2026-05-02 12:52:27 -0700

    Added session management and route protection. Displays logout on navbar when user is logged in

--- index.mjs
+++ index.mjs
@@ -1,6 +1,7 @@
 import express from 'express';
 import mysql from 'mysql2/promise';
 import bcrypt from 'bcrypt';
+import session from 'express-session';
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
@@ -19,16 +20,36 @@ const pool = mysql.createPool({
    waitForConnections: true
 });
 
+//Setting Session
+app.use(session({
+   //TODO: use in memory state - set session to expire when browser closes
+   secret: 'seceret', // Used to sign the session ID cookie
+   resave: false,             // Avoid resaving session if unmodified
+   saveUninitialized: false,  // Don't create session until something stored
+   cookie: {
+      secure: false,           // Set to true in production with HTTPS
+      // maxAge: expire when page closes   // Session expiry in milliseconds (1 minute)
+   }
+}));
+
+function isUserAuthenticated(req, res, next){
+    if(req.session.authenticated){
+        next();
+    }else{
+        res.redirect("/");//change to login page once created
+    }
+}
+
 app.get('/', (req, res) => {
-   res.render('home.ejs');
+   res.render('home.ejs', { user: req.session.username || null });
 });
 
 app.get('/signUp', (req, res) => {
-   res.render('signUp.ejs');
+   res.render('signUp.ejs', { user: req.session.username || null });
 });
 
-app.get('/createPost', (req, res) => {
-   res.render('createPost.ejs');
+app.get('/createPost', isUserAuthenticated, (req, res) => {
+   res.render('createPost.ejs', { user: req.session.username || null });
 });
 
 app.listen(3000, () => {
@@ -55,11 +76,29 @@ app.post('/createUser', async (req, res) => {
       let sql = `INSERT INTO users (username, email, password_hash) VALUES (?, ?, ?)`;
       await pool.query(sql, [username, email, hashedPassword]);
 
+      let sql2 = `SELECT id FROM users WHERE username = ?`;
+      let [rows] = await pool.query(sql2, [username]);
+
+      //Set Session
+      req.session.authenticated = true;
+      req.session.userId = rows[0].id;
+      req.session.username = username;
+
       res.json({ success: true });
    } catch (err) {
       console.error("Error creating user:", err);
       res.status(500).json({ success: false, error: err.message });
    }
+});
+
+app.get('/logout', (req, res) => {
+   req.session.destroy((err) => {
+      if (err) {
+         console.error("Logout error:", err);
+         return res.redirect('/');
+      }
+      res.redirect('/');  // Redirect to home after session destroyed
+   });
 });
 
 app.get("/dbTest", async (req, res) => {
